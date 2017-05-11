a80f33b3f39d42bcd286ece3a81adbd4956ba7cf
Dany Patrie <dany.patrie@gmail.com>
2017-05-10 22:41:17 -0400

    first revision

---
+++ rip/README.md
@@ -0,0 +1,38 @@
+#Automated script to create a musical quiz
+
+We read an input file. In the future we will read the file from Google Drive directly
+
+###To process a "normal" question
+1- Rip mp3 from youtube
+2- Cut mp3 for question and answer with ffmpeg
+
+###To process an "overlap" question
+1- Rip both mp3 from youtube
+2- Cut mp3 for question and answer with ffmpeg
+3- Mix them with sox (with optional volume adjustment)
+
+###To process a "midi" question
+1- Download midi file
+2- Convert to mp3 by piping it to ffmpeg
+3- Cut mp3 for question and answer with ffmpeg
+
+###To rip an mp3 from youtube
+```
+youtube-dl --extract-audio --audio-format mp3 '[youtube url]'
+```
+###To cut an mp3 with ffmpeg
+```
+ffmpeg -ss [start second] -i [mp3 file] -t [length] -acodec copy [final file]
+```
+
+###To convert a midi file to mp3
+```
+timidity [midi file] -Ow -o - | ffmpeg -i - -acodec libmp3lame -ab 64k [mp3 file]
+```
+
+###To mix to mp3 together
+```
+sox -m [first mp3] -v [optional volume adjustment] [second mp3] [final file]
+```
+
+
---
+++ rip/main.go
@@ -0,0 +1,292 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+var (
+	youtubedl, timidity, ffmpeg, sox string
+	tmpdir                           string
+	outdir                           string
+)
+
+func init() {
+	var err error
+	if youtubedl, err = exec.LookPath("youtube-dl"); err != nil {
+		panic("unable to find youtube-dl")
+	}
+	if timidity, err = exec.LookPath("timidity"); err != nil {
+		panic("unable to find timidity")
+	}
+	if ffmpeg, err = exec.LookPath("ffmpeg"); err != nil {
+		panic("unable to find ffmpeg")
+	}
+	if sox, err = exec.LookPath("sox"); err != nil {
+		panic("unable to find sox")
+	}
+	if tmpdir, err = ioutil.TempDir("", "quizrip_"); err != nil {
+		panic("unable to create temp dir")
+	}
+	log.Println("Using", tmpdir)
+}
+
+type csvline []string
+
+func (l csvline) Question() string {
+	if len(l[1]) < 2 {
+		return "0" + l[1]
+	}
+	return l[1]
+}
+func (l csvline) Type() string { return l[2] }
+func (l csvline) Titre() string {
+	return strings.NewReplacer(" - ", "-", " ", "-").Replace(l[3]) + ".mp3"
+}
+func (l csvline) URL() string     { return l[4] }
+func (l csvline) QStart() string  { return l[5] }
+func (l csvline) QLength() string { return l[6] }
+func (l csvline) AStart() string  { return l[7] }
+func (l csvline) ALength() string { return l[8] }
+func (l csvline) Speed() string   { return l[9] }
+func (l csvline) Path(prefix string) string {
+	return filepath.Join(
+		outdir,
+		prefix,
+		fmt.Sprintf(
+			"%s-%s",
+			l.Question(),
+			l.Titre(),
+		),
+	)
+}
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Println("usage: ./quizrip csvfile outputfolder")
+		os.Exit(1)
+	}
+
+	lines, err := readCSV(os.Args[1])
+	if err != nil {
+		fmt.Println("unable to read input file:", err)
+		os.Exit(1)
+	}
+	outdir = os.Args[2]
+	if err := os.MkdirAll(outdir, 0700); err != nil {
+		fmt.Println("unable to create output folder:", err)
+		os.Exit(1)
+	}
+	os.MkdirAll(filepath.Join(outdir, "questions"), 0700)
+	os.MkdirAll(filepath.Join(outdir, "reponses"), 0700)
+
+	lines = lines[1:] //Skipping headers
+	for _, l := range lines {
+		line := csvline(l)
+		switch line.Type() {
+		case "normal":
+			if err := processNormal(line); err != nil {
+				fmt.Printf("unable to process %s:%s", line.Titre(), err)
+			}
+		case "midi":
+			if err := processMidi(line); err != nil {
+				fmt.Printf("unable to process %s:%s", line.Titre(), err)
+			}
+		case "slow", "fast":
+			log.Println(l)
+			if err := processSpeed(line); err != nil {
+				fmt.Printf("unable to process %s:%s", line.Titre(), err)
+			}
+		}
+	}
+}
+
+func processMidi(l csvline) error {
+	midi, err := download(l.URL())
+	if err != nil {
+		return err
+	}
+
+	mp3, err := convertMidiToMp3(midi)
+	if err != nil {
+		return err
+	}
+
+	question, err := cutMp3(mp3, l.QStart(), l.QLength())
+	if err != nil {
+		return err
+	}
+	answer, err := cutMp3(mp3, l.AStart(), l.ALength())
+	if err != nil {
+		return err
+	}
+
+	if err := os.Rename(question, l.Path("questions")); err != nil {
+		return err
+	}
+	return os.Rename(answer, l.Path("reponses"))
+}
+func processSpeed(l csvline) error {
+	if err := processNormal(l); err != nil {
+		return err
+	}
+	mp3, err := changeSpeed(l.Path("questions"), l.Speed())
+	if err != nil {
+		return err
+	}
+	return os.Rename(mp3, l.Path("questions"))
+}
+
+func processNormal(l csvline) error {
+	mp3, err := ripYoutubeMp3(l.URL())
+	if err != nil {
+		return err
+	}
+
+	question, err := cutMp3(mp3, l.QStart(), l.QLength())
+	if err != nil {
+		return err
+	}
+	answer, err := cutMp3(mp3, l.AStart(), l.ALength())
+	if err != nil {
+		return err
+	}
+
+	if err := os.Rename(question, l.Path("questions")); err != nil {
+		return err
+	}
+	return os.Rename(answer, l.Path("reponses"))
+}
+
+func readCSV(infile string) ([][]string, error) {
+	f, err := os.Open(infile)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	r := csv.NewReader(f)
+	return r.ReadAll()
+}
+
+func tempfile() *os.File {
+	f, err := ioutil.TempFile(tmpdir, "")
+	if err != nil {
+		panic(err)
+	}
+	return f
+}
+
+func tempname(sufix string) string {
+	f := tempfile()
+	f.Close()
+	os.Remove(f.Name())
+	return f.Name() + sufix
+}
+
+func tempdir() string {
+	dir, err := ioutil.TempDir(tmpdir, "")
+	if err != nil {
+		panic(err)
+	}
+	return dir
+}
+
+func command(name string, args ...string) *exec.Cmd {
+	log.Printf("Running %s %s", name, strings.Join(args, " "))
+	cmd := exec.Command(name, args...)
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	return cmd
+}
+
+func download(url string) (outfile string, err error) {
+	var resp *http.Response
+	if resp, err = http.Get(url); err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	f := tempfile()
+	defer f.Close()
+	if _, err = io.Copy(f, resp.Body); err != nil {
+		return
+	}
+	outfile = f.Name()
+
+	return
+}
+
+//youtube-dl --extract-audio --audio-format mp3 '[youtube url]'
+func ripYoutubeMp3(url string) (outfile string, err error) {
+	dir := tempdir()
+	os.Chdir(dir)
+
+	if err = command(youtubedl, "--extract-audio", "--audio-format", "mp3", url).Run(); err != nil {
+		return
+	}
+	outfile = tempname(".mp3")
+	filepath.Walk(dir, func(name string, info os.FileInfo, err error) error {
+		if !info.IsDir() {
+			os.Rename(name, outfile)
+			log.Println("outfile from youtube is", name)
+		}
+		return nil
+	})
+
+	return
+}
+
+//ffmpeg -ss [start second] -i [mp3 file] -t [length] -acodec copy [final file]
+func cutMp3(infile, start, length string) (outfile string, err error) {
+	os.Chdir(tmpdir)
+	outfile = tempname(".mp3")
+
+	err = command(ffmpeg, "-ss", start, "-i", infile, "-t", length, "-acodec", "copy", outfile).Run()
+	return
+}
+
+//timidity [midi file] -Ow -o - | ffmpeg -i - -acodec libmp3lame -ab 64k [mp3 file]
+func convertMidiToMp3(infile string) (outfile string, err error) {
+	os.Chdir(tmpdir)
+	outfile = tempname(".mp3")
+
+	c1 := command(timidity, infile, "-Ow", "-o", "-")
+	c2 := command(ffmpeg, "-i", "-", "-acodec", "libmp3lame", "-ab", "64k", outfile)
+
+	r, w := io.Pipe()
+	c1.Stdout = w
+	c2.Stdin = r
+
+	c1.Start()
+	c2.Start()
+	c1.Wait()
+	w.Close()
+	c2.Wait()
+
+	return
+}
+
+//sox -m [first mp3] -v [volume adjustment] [second mp3] [final file]
+func mixMp3(file1, file2, volumeAdjust string) (outfile string, err error) {
+	os.Chdir(tmpdir)
+	outfile = tempname(".mp3")
+	err = command(sox, file1, "-v", volumeAdjust, file2, outfile).Run()
+	return
+}
+
+//sox [infile] [outfile] speed [factor]
+func changeSpeed(infile, speed string) (outfile string, err error) {
+	os.Chdir(tmpdir)
+	outfile = tempname(".mp3")
+	err = command(sox, infile, outfile, "speed", speed).Run()
+	return
+}
